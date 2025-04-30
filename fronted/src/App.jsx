13adea92007d512b8d15e0a49e2da26e860fd1a2
tharushi lakshani <tharushilakshani81@gmail.com>
2025-04-30 19:37:31 +0530

    refactor(App.jsx): organize imports and clean up route definitions

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-// Your page imports
+import Register from './pages/Register';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Accomadations from './pages/Accomadations';
+import Attractions from './pages/Attractions';
+import Plan from './pages/plan';
+import Place from './pages/Place';
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Managing the logged-in state
@@ -20,10 +27,16 @@ function App() {
       <Router>
         <Routes>
           {/* Unprotected Routes */}
-          
+          <Route path="/register" element={<Register/>}/>
           <Route path="/" element={<Home />} />
-          \
-          
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/accomadations" element={<Accomadations/>}/>
+          <Route path="/attractions/:placeID" element={<Place/>}/>
+          <Route path="/plan" element={<Plan/>} />
+          <Route path="/place" element={<Place/>}/>
+          <Route path="/attractions" element={<Attractions/>}></Route>
           {/* You can add more protected routes here */}
         </Routes>
       </Router>
